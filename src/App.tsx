2b062282818c0e7826938b7212c0bb3da317f9f7
Andre Alves <andrealvesdeoliveira@outlook.com>
2020-08-24 16:54:03 -0300

    Configurado função para pegar a localização do navegador

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 import HomeBuilding from './components/HomeBuilding';
 import GlobalStyle from './styles/global'
 import light from './styles/themes/light';
 import dark from './styles/themes/dark';
 import Sunset from './components/Sunset';
+import api from './services/api'
+import axios from 'axios';
 
 function App() {
 
+  /* Função para pegar location do browser */
+  const [location, setLocation] = useState(false);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position)=>{
+      console.log(position.coords.latitude, position.coords.longitude);
+      setLocation(true)
+    })
+  }, [])
+
+  /* Função para ativar darktheme ao clicar no sun */
   const [theme, setDarkTheme] = useState(light);
 
   const toggleTheme = () => {
@@ -15,13 +28,15 @@ function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <GlobalStyle />
-        <Sunset toggleTheme={ toggleTheme } />
-        <HomeBuilding />
-      </div>
-    </ThemeProvider>
+    <Fragment>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <GlobalStyle />
+          <Sunset toggleTheme={ toggleTheme } />
+          <HomeBuilding />
+        </div>
+      </ThemeProvider>
+    </Fragment>
   );
 }
 
